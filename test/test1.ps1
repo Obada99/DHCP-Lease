b7caab8dc130a454a499cd43b8e765300fa37b0c
Obada99 <64024525+Obada99@users.noreply.github.com>
2021-10-22 09:52:32 +0200

    DHCP-Lease script

---
+++ test/test1.ps1
@@ -0,0 +1,18 @@
+﻿$names=Get-Content "C:\Users\adm_oabazid\Desktop\test\test.txt"
+foreach($name in $names) {
+
+
+if(Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+
+Write-Host "$name is up" -ForegroundColor Green
+$output+="$name is up,"+"`n"
+
+}
+else{
+Write-Host "$name is down" -ForegroundColor Red
+$output+="$name is down,"+"`n"
+Remove-DhcpServerv4Lease -IPAddress $name
+
+}
+}
+$output | Out-File "C:\Users\adm_oabazid\Desktop\test\result.txt"
